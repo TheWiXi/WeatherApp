645ee8ded694e2025e88a92078db1ec7818b63aa
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-10-22 01:32:59 -0500

    feat: :sparkles: Se agrega componente con la informacion general de la ciudad en la fecha actual

---
+++ weather-app/src/components/weather/CurrentWeader/CurrentWeather.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Wind, Droplets, Gauge, Sun } from 'lucide-react';
+
+
+const date = new Date();
+
+  const months = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+  ];
+
+  const month = months[date.getMonth()]; // Mes
+  const day = date.getDate(); // Día del mes
+  const hours = date.getHours(); // Hora
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+const CurrentWeather = ({ data }) => {
+  const current = data.current;
+  const location = data.location;
+  if (!data) return null;
+
+  return (
+    <div className="relative overflow-hidden rounded-3xl text-white bg-gradient-to-b from-purple-600 to-purple-900 p-6 pt-28">
+      <div className="flex justify-between items-start mb-8">
+        <div>
+          <h2 className="text-6xl font-bold">{Math.round(current.temp_c)}°</h2>
+          <p className="text-xl mt-2">Feels like {Math.round(current.feelslike_c)}°</p>
+        </div>
+        <div className="text-right">
+          <img src={current.condition.icon} alt={current.condition.text} className="w-20 h-20" />
+          <p className="mt-2 text-xl">{current.condition.text}</p>
+        </div>
+      </div>
+      <div className='flex justify-between items-center'>
+        <h3>{month} {day}, {hours}:{minutes}</h3>
+        <div className="flex flex-col mt-4">
+          <p className="text-sm">
+            Day {Math.round(data.forecast.forecastday[0].day.maxtemp_c)}° 
+          </p>
+          <p className="text-sm">
+            Night {Math.round(data.forecast.forecastday[0].day.mintemp_c)}°
+          </p>
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-black/50 to-transparent"></div>
+    </div>
+  );
+};
+
+export default CurrentWeather;
